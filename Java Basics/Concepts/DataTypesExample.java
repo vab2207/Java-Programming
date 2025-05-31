69b480d2f862b9565f9731007fa257fb4d821194
vab2207 <aditya2207vab@gmail.com>
2025-05-31 22:20:33 +0530

    Initial commit

--- Java Basics/Concepts/DataTypesExample.java
+++ Java Basics/Concepts/DataTypesExample.java
@@ -1,9 +1,9 @@
 public class DataTypesExample {
     public static void main(String[] args) {
-        int age =a 25;
+        int age = 25;
         double salary = 55000.50;
         char grade = 'A';
         boolean isPassed = true;
-        System.out.println(age +"/n"+ salery +"/n"+ grade + "/n"+ isPassed);
+        System.out.println(age +"\n"+ salary +"\n"+ grade + "\n"+ isPassed);
     }
 }
